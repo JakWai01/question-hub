ca23bf3a887612771fe7d7904123b67e5bc7c4e6
JakWai01 <salet@posteo.net>
2023-12-10 20:15:49 +0100

    feat: Add initial server listener

--- server/main.py
+++ server/main.py
@@ -1,16 +1,67 @@
 import socket
+from ipaddress import IPv4Interface
+import netifaces
+from enum import Enum, unique
+from threading import Thread
 
-HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
-PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
+def is_valid(address: str, broadcast: str | None):
+    if not broadcast:
+        return False
+    if "." not in address:
+        return False
+    if address == "127.0.0.1":
+        return False
+    return True
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
+
+def get_network_interface() -> IPv4Interface:
+    for interface in reversed(netifaces.interfaces()):
+        for addr, *_ in netifaces.ifaddresses(interface).values():
+            address = addr.get("addr", "")
+            broadcast = addr.get("broadcast")
+            if is_valid(address, broadcast):
+                netmask = addr.get("netmask")
+                return IPv4Interface(f"{address}/{netmask}")
+    raise Exception("Cannot find network interface to listen on")
+
+# hostname and network interface
+HOSTNAME = socket.gethostname()
+INTERFACE = get_network_interface()
+
+BROADCAST_PORT = 34567
+BROADCAST_IP = str(INTERFACE.network.broadcast_address)
+
+def register_listener(callback):
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(('', BROADCAST_PORT))
+
+    try:
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+            data, (ip, port) = listen_socket.recvfrom(1024)
+            if data:
+                message = data.decode("utf-8")
+                callback(message, ip, port)
+    except KeyboardInterrupt:
+        listen_socket.close()
+        exit(0)
+
+def callback(message, ip, port):
+    print(f"Broadcast message received: {message}") 
+       
+def main():
+    threads = []
+    
+    listener_thread = Thread(target=register_listener, args=(callback,))
+    listener_thread.start()
+    threads.append(listener_thread)
+
+    for thread in threads:
+        thread.join()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit(0)
