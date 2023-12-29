f5475fddba991d6b5e028a5b509344afd2ababaa
Mouad Abuaisha <mouad.abuaisha@gmail.com>
2023-12-29 14:04:15 +0100

    added user ui setup and http server in client

--- client/main.py
+++ client/main.py
@@ -1,12 +1,30 @@
+import http.server
+import socketserver
 import socket
+import threading
+import netifaces
+from ipaddress import IPv4Interface
 
-HOST = "host.containers.internal"  # The server's hostname or IP address
-PORT = 8000 # The port used by the server
+# HTTP server to handle web client requests
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    pass
+
+def http_server():
+    PORT = 8000
+    Handler = MyHttpRequestHandler
+
+    with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
+        print("HTTP server started on port", PORT)
+        httpd.serve_forever()
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+#Client Node Initiator
 
-print(f"Received {data!r}", flush=True)
+if __name__ == "__main__":
+    try:
+        # Start the HTTP server in a separate thread
+        http_thread = threading.Thread(target=http_server)
+        http_thread.start()
+        
+    except Exception as e:
+        print(f"Error: {e}")
---
+++ qhub-ui/src/components/questions.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="text-center fill-height">
+      <h1>Question Hub</h1>
+
+      <div>
+        <v-expansion-panels class="mb-6">
+
+        <v-expansion-panel v-for="question in questions" :key="question">
+
+          <div class="d-flex">
+
+            <div class="d-flex flex-column pt-2">
+              <h3>{{ question.order }}</h3>
+              <v-btn class="ma-1" size="small" variant="text" icon="mdi-thumb-up" color="blue-lighten-2"></v-btn>
+            </div>
+
+            <v-expansion-panel-title expand-icon="mdi-menu-down">
+              {{ question.title }}
+            </v-expansion-panel-title>
+
+          </div>
+          
+
+          <v-expansion-panel-text>
+            {{ question.message }}
+          </v-expansion-panel-text>  
+
+        </v-expansion-panel>
+
+        </v-expansion-panels>
+      </div>
+
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const questions = ref(
+  [
+    { title: 'Python3 Issues', message: 'I cant run python3 commands on shell', order: 0 },
+    { title: 'Python2 Issues', message: 'I cant run python3 commands on shell', order: 0 },
+    { title: 'Python1 Issues', message: 'I cant run python3 commands on shell', order: 0 },
+    { title: 'Python0 Issues', message: 'I cant run python3 commands on shell', order: 0 },
+  ])
+
+</script>
