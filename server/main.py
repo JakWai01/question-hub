44a54d323af907e5fd6a24aaf1cb0df2f0e0f5d8
JakWai01 <salet@posteo.net>
2024-01-08 18:49:47 +0100

    feat: Replace broadcast with unicast IPs in nodes

--- server/main.py
+++ server/main.py
@@ -21,7 +21,54 @@ class Node:
         
     def __hash__(cls):
         return hash(f"{cls.ip}:{cls.port}") 
-       
+ 
+    def broadcast_target(cls, callback):
+        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listen_socket.bind(('', BROADCAST_PORT))
+
+        try:
+            while True:
+                data, (ip, port) = listen_socket.recvfrom(1024)
+                if data:
+                    msg = Message.unmarshal(data)
+                    callback(msg, ip)
+        except KeyboardInterrupt:
+            listen_socket.close()
+            exit(0)
+
+    def unicast_target(cls, callback, lport: int):
+        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        listen_socket.bind((INTERFACE.ip.compressed, lport))
+
+        try: 
+            while True:
+                data, (ip, port) = listen_socket.recvfrom(1024)
+                if data:
+                    msg = Message.unmarshal(data)
+                    callback(msg, ip)
+        except KeyboardInterrupt:
+            listen_socket.close()
+            exit(0)
+
+    def heartbeat_target(cls, callback, delay: int):
+        try:
+            while True:
+                new_hb_dict = cp._node_heartbeats.copy()
+                for socket, hb in cp._node_heartbeats.items():
+                    if hb + 2 < int(time.time()):
+                        node = cp.get_node_from_socket(socket)
+                        cp.remove_node(node)
+                        new_hb_dict.pop(socket)
+                        if node.leader is True:
+                            Message(opcode=OpCode.ELECTION).broadcast()
+                cp._node_heartbeats = new_hb_dict
+                Message(opcode=OpCode.HEARTBEAT, port=cls.port).broadcast()
+                time.sleep(delay)
+                
+        except KeyboardInterrupt:
+            exit(0)      
     
 class ControlPlane:
     def __init__(self):
@@ -128,81 +175,32 @@ BROADCAST_PORT = 34567
 BROADCAST_IP = str(INTERFACE.network.broadcast_address)
 cp = ControlPlane()
 
-def broadcast_target(callback):
-    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    listen_socket.bind(('', BROADCAST_PORT))
-
-    try:
-        while True:
-            data, (ip, port) = listen_socket.recvfrom(1024)
-            if data:
-                msg = Message.unmarshal(data)
-                callback(msg, ip, port)
-    except KeyboardInterrupt:
-        listen_socket.close()
-        exit(0)
-
-def unicast_target(callback, lport: int):
-    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    listen_socket.bind((INTERFACE.ip.compressed, lport))
-
-    try: 
-        while True:
-            data, (ip, port) = listen_socket.recvfrom(1024)
-            if data:
-                msg = Message.unmarshal(data)
-                callback(msg, ip, port)
-    except KeyboardInterrupt:
-        listen_socket.close()
-        exit(0)
-
-def heartbeat_target(callback, delay: int):
-    try:
-        while True:
-            new_hb_dict = cp._node_heartbeats.copy()
-            for socket, hb in cp._node_heartbeats.items():
-                if hb + 2 < int(time.time()):
-                    node = cp.get_node_from_socket(socket)
-                    cp.remove_node(node)
-                    new_hb_dict.pop(socket)
-                    if node.leader is True:
-                        Message(opcode=OpCode.ELECTION).broadcast()
-            cp._node_heartbeats = new_hb_dict
-            Message(opcode=OpCode.HEARTBEAT).broadcast()
-            time.sleep(delay)
-            
-    except KeyboardInterrupt:
-        exit(0)
-
-
-def hello_handler(message: Message, ip: str, port: int):
-    cp.register_node(Node(ip, port))
+def hello_handler(message: Message, ip: str):
+    cp.register_node(Node(ip, message.port))
     Message(opcode=OpCode.HELLO_REPLY, data=list(map(lambda node: node.__dict__, cp.nodes))).send(ip, message.port)
 
-def hello_reply_handler(message: Message, ip: str, port: int):
+def hello_reply_handler(message: Message, ip: str):
     print("Received node state")
     print(message)
 
-def heartbeat_handler(message: Message, ip: str, port: int):
-    print(f"Received heartbeat from {ip}:{port}")
-    cp.register_heartbeat(f"{ip}:{port}")
+def heartbeat_handler(message: Message, ip: str):
+    print(f"Received heartbeat from {ip}:{message.port}")
+    cp.register_heartbeat(f"{ip}:{message.port}")
     print(cp._node_heartbeats)
 
-def election_handler(message: Message, ip: str, port: int):
-    print(f"Received election from {ip}:{port}")
+def election_handler(message: Message, ip: str):
+    print(f"Received election from {ip}:{message.port}")
         
-def message_handler(message: Message, ip: str, port: int):
-    print(f"Broadcast message received: {message} from {ip}:{port}", flush=True)
+def message_handler(message: Message, ip: str):
+    print(f"Broadcast message received from {ip}:{message.port}", flush=True)
     if message.opcode is OpCode.HELLO:
-        hello_handler(message, ip, port)        
+        hello_handler(message, ip)        
     elif message.opcode is OpCode.HELLO_REPLY:
-        hello_reply_handler(message, ip, port)
+        hello_reply_handler(message, ip)
     elif message.opcode is OpCode.HEARTBEAT:
-        heartbeat_handler(message, ip, port)
+        heartbeat_handler(message, ip)
     elif message.opcode is OpCode.ELECTION:
-        election_handler(message, ip, port)
+        election_handler(message, ip)
     else:
         return
     print(cp.nodes)
@@ -220,20 +218,22 @@ def main():
     print(BROADCAST_IP)
    
     threads = []
-    
-    listener_thread = Thread(target=broadcast_target, args=(message_handler,))
+
+    node = Node(INTERFACE.ip.compressed, args.port)
+
+    listener_thread = Thread(target=node.broadcast_target, args=(message_handler,))
     listener_thread.start()
     threads.append(listener_thread)
 
-    uni_thread = Thread(target=unicast_target, args=(message_handler, args.port))
+    uni_thread = Thread(target=node.unicast_target, args=(message_handler, args.port))
     uni_thread.start()
     threads.append(uni_thread)
 
-    heartbeat_thread = Thread(target=heartbeat_target, args=(message_handler, args.delay))
+    heartbeat_thread = Thread(target=node.heartbeat_target, args=(message_handler, args.delay))
     heartbeat_thread.start()
     threads.append(heartbeat_thread)
     
-    Message(OpCode.HELLO, port=args.port).broadcast(2)
+    Message(OpCode.HELLO, port=node.port).broadcast(2)
 
     for thread in threads:
         thread.join()
